2f82c7cbecf9d8a2667d333a10ff419f322dce50
Eric Ebulu <ekumamait@Eric-2.local>
2019-08-20 11:01:29 +0200

    ft(Queries): implement subcriptions
    - Add subscriptions
    - Wrap the app with Apollo Provider so as to access GraphQL data
    
    [Delivers #167064107]

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000/'
+  });
+  
+  const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+      reconnect: true
+    }
+  });
+  
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+  );
+  
+  const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+  })
+
+ReactDOM.render(  
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+  document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister() to register() below. 
 serviceWorker.unregister();
